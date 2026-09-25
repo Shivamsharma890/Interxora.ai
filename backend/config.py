04f13501caf128972789fc619b81bf3ef4721c0e
Shivam Sharma <ss7614103@gmail.com>
2026-09-25 23:12:00 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,33 @@
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
+# ACCESS_TOKEN_EXPIRE_MINUTES = int(
+#     os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
+# )
+
+# GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+# GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+
+# if not DATABASE_URL:
+#     raise ValueError("DATABASE_URL is not configured")
+
+# if not SECRET_KEY:
+#     raise ValueError("SECRET_KEY is not configured")
+
+# if not GOOGLE_CLIENT_ID:
+#     raise ValueError("GOOGLE_CLIENT_ID is not configured")
+
+# if not GOOGLE_CLIENT_SECRET:
+#     raise ValueError("GOOGLE_CLIENT_SECRET is not configured")
+
+#.................................new................................
 import os
 from dotenv import load_dotenv
 
@@ -26,3 +56,4 @@ if not GOOGLE_CLIENT_ID:
 
 if not GOOGLE_CLIENT_SECRET:
     raise ValueError("GOOGLE_CLIENT_SECRET is not configured")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173").rstrip("/")
